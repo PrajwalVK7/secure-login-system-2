36eef7c05ac1ea7593bf6a9d95ec7e29fc798d70
PrajwalVK7 <prajwalvk98@gmail.com>
2024-03-23 17:56:24 +0530

    styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,16 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 function Home() {
   return (
-    <>
-      <Header />
-      <div className='container'>
+    <div className=' home' style={{height:'100vh'}}>
+       <Header />
+      <div className='container home'>
         <h1 className='text-center'>Lorem, ipsum dolor.</h1>
         <div className='mt-5 mb-5'>
           <Row>
             <Col lg={6} md={6}>
               <div>
                 <Link to={'/event'} style={{textDecoration:'none'}}>
-                <Card className='d-flex justify-content-center shadow align-items-center' style={{ width: '100%', height: '20rem' }}>
+                <Card className='btn btn-warning d-flex justify-content-center shadow align-items-center' style={{ width: '100%', height: '20rem', }}>
                   <div>
                     <Card.Body>
                       <Card.Title>Upload Your Content,& Get a chance To win</Card.Title>
@@ -29,27 +29,13 @@ function Home() {
               </div>
             </Col>
             <Col lg={6} md={6}>
-              <div>
-                <Card className='d-flex justify-content-center align-items-center' style={{ width: '100%', height: '20rem' }}>
-                  <div>
-                    <Card.Body>
-                      <Card.Title>Card Title</Card.Title>
-                      <Card.Text>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum provident est excepturi beatae eaque minus, mollitia accusamus hic cupiditate vero tempore assumenda molestiae ea, doloremque cumque minima in repellat culpa!
-                      </Card.Text>
-                      <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                  </div>
-                </Card>
-              </div>
             </Col>
 
 
           </Row>
         </div>
       </div>
-
-    </>
+    </div>
   )
 }
 
